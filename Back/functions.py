bf3f540acb20dce3b72bbc0e5f522b9da035593b
Vinícius Gadioli <viniciusgadioli2@gmail.com>
2023-10-02 18:09:10 -0300

    Commit1.3BACK
    Agora resolver o clientes_match

--- Back/functions.py
+++ Back/functions.py
@@ -1,8 +1,7 @@
 import openpyxl
-import dropbox
 from twilio.rest import Client
 from datetime import datetime, timedelta
-import io
+
 
 
 # Inicialização da lista global de dados
@@ -15,6 +14,7 @@ lista_parcelas = []
 lista_valor_parcelas = []
 lista_valor_total = []
 lista_data_cadastro = []
+
 
 # Função para coletar os dados do cliente
 def coletar_dados_cliente():
@@ -30,14 +30,16 @@ def coletar_dados_cliente():
     porcentagem = int(input("Digite o valor da porcentagem de juros: "))
     lista_porcentagem.append(porcentagem)
 
-    intervalo_pagamentos = int(input("Digite a cada quanto tempo o cliente irá pagar (em dias): "))
+    intervalo_pagamentos = int(
+        input("Digite a cada quanto tempo o cliente irá pagar (em dias): ")
+    )
     lista_pagamento.append(intervalo_pagamentos)
 
     parcelas = int(input("Digite o número de parcelas: "))
     lista_parcelas.append(parcelas)
 
     valor_total = valor
-    valor_parcela = (valor_total / parcelas) * (1 + porcentagem / 100)
+    valor_parcela = (valor_total / parcelas) + (porcentagem / 100 * valor)
     formatted_valor_parcela = round(valor_parcela, 2)
     lista_valor_parcelas.append(formatted_valor_parcela)
 
@@ -45,29 +47,43 @@ def coletar_dados_cliente():
     lista_valor_total.append(total_a_pagar)
 
     data_hoje = datetime.now()
-    data_formatada = data_hoje.strftime("%d/%m/%Y")  # Modificado para incluir o ano com quatro dígitos
+    data_formatada = data_hoje.strftime(
+        "%d/%m/%Y"
+    )  # Modificado para incluir o ano com quatro dígitos
     lista_data_cadastro.append(data_formatada)
 
     # Retorne os valores relevantes
-    return cliente, nmr_cliente, valor, porcentagem, intervalo_pagamentos, parcelas, formatted_valor_parcela, total_a_pagar, data_formatada
+    return (
+        cliente,
+        nmr_cliente,
+        valor,
+        porcentagem,
+        intervalo_pagamentos,
+        parcelas,
+        formatted_valor_parcela,
+        total_a_pagar,
+        data_formatada,
+    )
 
-# Função para fazer upload para o Dropbox
+
+
 def adicionar_dados_planilha_local():
-    # Nome do arquivo da planilha na pasta do projeto
-    nome_arquivo = 'clientes.xlsx'
+    nome_arquivo = "clientes.xlsx"
 
-    # Tente carregar a planilha existente
     try:
         workbook = openpyxl.load_workbook(nome_arquivo)
     except FileNotFoundError:
-        # Se a planilha não existir, crie uma nova
         workbook = openpyxl.Workbook()
 
-    # Obtenha a folha ativa
     sheet = workbook.active
+    def find_next_empty_row(sheet):
+        for row in range(1, sheet.max_row + 1):
+            if sheet.cell(row=row, column=1).value is None:
+                return row
+        return sheet.max_row + 1
 
-    # Encontre a próxima linha vazia na planilha
-    proxima_linha = sheet.max_row + 1
+    # Encontrar a próxima linha vazia
+    proxima_linha = find_next_empty_row(sheet)
 
     # Adicione os dados dos clientes à planilha a partir da próxima linha vazia
     for i in range(len(lista_clientes)):
@@ -83,135 +99,68 @@ def adicionar_dados_planilha_local():
 
     # Salve a planilha atualizada na pasta do projeto
     workbook.save(nome_arquivo)
+    workbook.close()
 
 
-def calculo_parcelas():
-    # Nome do arquivo da planilha na pasta do projeto
-    nome_arquivo = 'clientes.xlsx'
+def identificar_parcelas():
+    nome_arquivo = "clientes.xlsx"
 
-    # Tente carregar a planilha existente
     try:
         workbook = openpyxl.load_workbook(nome_arquivo)
     except FileNotFoundError:
         print("A planilha 'clientes.xlsx' não foi encontrada.")
-        return []
+        exit(1)
 
-    # Obtenha a folha ativa
     sheet = workbook.active
 
-    # Data de hoje
+    if sheet.max_row < 2:
+        print("A planilha não possui dados suficientes.")
+        exit(1)
+
     data_atual = datetime.now().date()
+    data_atual_formatada = data_atual.strftime("%d/%m/%y")
 
-    # Lista para armazenar as informações de envio de SMS
-    infos_envio_sms = []
+    clientes_match = []
 
-    # Percorra as linhas da planilha
     for row in sheet.iter_rows(min_row=2, values_only=True):
-        data_cadastro_str = row[8]  # Coluna 9 contém a data de cadastro
-
-        # Verifique se o valor da célula é None ou vazio
-        if data_cadastro_str is None or data_cadastro_str == "":
-            print("Data de cadastro ausente ou vazia.")
+        if row[8] is None or row[4] is None or row[4] == 0 or row[5] is None or row[6] is None:
             continue
 
-        # Verifique se o valor da célula é um objeto datetime
-        if isinstance(data_cadastro_str, datetime):
-            data_cadastro = data_cadastro_str.date()  # Converta para data se for datetime
-        else:
-            data_cadastro_str = str(data_cadastro_str)
-            try:
-                data_cadastro = datetime.strptime(data_cadastro_str, "%d/%m/%Y").date()  # Sempre com AAAA
-            except ValueError:
-                print(f"Erro na data de cadastro: '{data_cadastro_str}'. Verifique o formato (DD/MM/AAAA).")
-                continue
+        nome_cliente = row[0]
+        celular_cliente = row[1]
+        valor_parcela = row[6]  # Ajustado para pegar o valor da parcela da coluna G
+        intervalo_pagamento = row[4]
+        total_parcelas = row[5]
 
-        intervalo_pagamento = row[4]  # Coluna 5 contém o intervalo de pagamento em dias
-        nome_cliente = row[0]  # Coluna 1 contém o nome do cliente
-        telefone_cliente = row[1]  # Coluna 2 contém o telefone do cliente
-        valor_parcela = row[6]  # Coluna 7 contém o valor da parcela
+        data_cadastro_dt = row[8].date() if isinstance(row[8], datetime) else row[8]
+        dias_desde_cadastro = (data_atual_formatada - data_cadastro_dt).days
+        total_dias_para_pagar = intervalo_pagamento * total_parcelas
 
-        # Calcule a diferença de meses desde a data de cadastro até a data atual
-        meses_desde_cadastro = (data_atual.year - data_cadastro.year) * 12 + (data_atual.month - data_cadastro.month)
+        if 0 < dias_desde_cadastro <= total_dias_para_pagar and dias_desde_cadastro % intervalo_pagamento == 0:
+            numero_parcela = dias_desde_cadastro // intervalo_pagamento
+            clientes_match.append({
+                'Nome': nome_cliente,
+                'Celular': celular_cliente,
+                'Parcela': numero_parcela,
+                'Valor da Parcela': valor_parcela  # Ajustado para usar o valor da parcela
+            })
 
-        # Verifique se a parcela deve ser paga hoje (mês seguinte ao cadastro)
-        if meses_desde_cadastro % intervalo_pagamento == 0:
-            numero_parcela = meses_desde_cadastro // intervalo_pagamento + 1
-            infos_envio_sms.append((nome_cliente, telefone_cliente, valor_parcela, numero_parcela))
+    workbook.close()
+    return clientes_match
 
-    return infos_envio_sms
 
-def sms_planilha_info():
-    # Configurar suas credenciais do Twilio
-    account_sid = 'ACae6b3430341fde63009bb4ccb9881310'
-    auth_token = '0c517bd637320f5e88532f7ad523f3b9'
-    twilio_phone_number = '+14783304454'
+def enviar_sms_cliente(nome_cliente, numero_celular, numero_parcela, valor_parcela):
+    account_sid = "ACae6b3430341fde63009bb4ccb9881310"
+    auth_token = "0c517bd637320f5e88532f7ad523f3b9"
+    twilio_phone_number = "+14783304454"
     client_twilio = Client(account_sid, auth_token)
 
-    # Configurar suas credenciais do Dropbox
-    access_token_dropbox = 'sl.Bm8WoIrwop3ju77mTCaTV-4zjZgSACEek2HAVGO_LkKN7mQthp-iho3ERaTxdgJlqu0XZewqQ8sCfQvatsbbFDVuo0hida5T-YTYnJmNCH6NZvvZqr9OQuQ-3xkDio-SbMVAwMCf1HWFcJfXehjBv5I'
-    dbx = dropbox.Dropbox(access_token_dropbox)
+    mensagem = f"Olá {nome_cliente}, hoje expira a data de pagamento da sua {numero_parcela}° parcela no valor de R${valor_parcela}."
 
-    # Nome do arquivo da planilha no Dropbox
-    nome_arquivo_dropbox = '/clientes.xlsx'
-
-    # Baixar a planilha do Dropbox e lê-la com o openpyxl
-    _, response = dbx.files_download(nome_arquivo_dropbox)
-    conteudo_planilha = response.content
-
-    # Abrir a planilha usando o openpyxl
-    planilha = openpyxl.load_workbook(io.BytesIO(conteudo_planilha), data_only=True)
-    sheet = planilha.active
-
-    # Inicializar a lista de dados dos clientes
-    dados_clientes = []
-
-    # Ler os dados da planilha a partir da segunda linha (pulando o cabeçalho)
-    for row in sheet.iter_rows(min_row=2, values_only=True):
-        dados_clientes.append(row)
-
-    # Data atual
-    data_hoje = datetime.now()
-
-    # Mostrar as entradas dos clientes
-    for idx, row in enumerate(dados_clientes, start=2):
-        nome_cliente, numero_celular, _, _, _, parcela, valor_parcela, _, data_vencimento = row
-        data_vencimento = datetime.strptime(data_vencimento, '%d/%m/%y')  # Corrigir a formatação da data
-        print(f"{idx - 1}. Nome: {nome_cliente}, Celular: {numero_celular}, Parcela: {parcela}, Valor: R${valor_parcela:.2f}, Vencimento: {data_vencimento.strftime('%d/%m/%Y')}")
-
-    # Solicitar a seleção do cliente
-    while True:
-        try:
-            indice_cliente = int(input("Digite o índice do cliente para enviar a mensagem (0 para sair): "))
-            if 1 <= indice_cliente <= len(dados_clientes):
-                break
-            else:
-                print("Índice inválido. Por favor, escolha um índice válido.")
-        except ValueError:
-            print("Entrada inválida. Digite um número válido.")
-
-    if indice_cliente == 0:
-        print("Saindo do programa.")
-        return
-
-    # Cliente selecionado
-    nome_cliente, numero_celular, _, _, _, parcela, valor_parcela, _, data_vencimento = dados_clientes[indice_cliente - 1]
-    data_vencimento = datetime.strptime(data_vencimento, '%d/%m/%y')  # Corrigir a formatação da data
-
-    # Calcular as datas de envio de SMS apenas para a próxima parcela a vencer
-    datas_envio_sms = calculo_parcelas(data_vencimento, int(parcela), int(parcela))
-    data_proxima_parcela = datas_envio_sms[0]
-
-    # Enviar a mensagem de SMS para a próxima parcela a vencer
-    data_envioSMS_formatada = data_proxima_parcela.strftime("%d/%m/%Y")
-    mensagem = f"Olá {nome_cliente}, você está recebendo um aviso em relação à próxima parcela no valor de R${valor_parcela:.2f} com vencimento em {data_envioSMS_formatada}. Providencie o pagamento."
-
-    # Enviar a mensagem de SMS
     message = client_twilio.messages.create(
         body=mensagem,
         from_=twilio_phone_number,
         to='+' + numero_celular
     )
-
-    print(f"Mensagem enviada para {nome_cliente}: {message.sid}")
-
+    print(f"Mensagem enviada para {nome_cliente} ({numero_celular}): {message.sid}")
 
--- Back/main.py
+++ Back/main.py
@@ -1,20 +1,26 @@
-from functions import coletar_dados_cliente, adicionar_dados_planilha_local, calculo_parcelas, sms_planilha_info
-from datetime import datetime, timedelta
+from functions import coletar_dados_cliente, adicionar_dados_planilha_local, identificar_parcelas, enviar_sms_cliente
 
 if __name__ == "__main__":
     # Colete os dados do cliente
-    #cliente, nmr_cliente, valor, porcentagem, intervalo_pagamentos, parcelas, formatted_valor_parcela, total_a_pagar, data_formatada = coletar_dados_cliente()
+    cliente, nmr_cliente, valor, porcentagem, intervalo_pagamentos, parcelas, formatted_valor_parcela, total_a_pagar, data_formatada = coletar_dados_cliente()
+
 
     # Adicione os dados à planilha local
-    #adicionar_dados_planilha_local()
+    adicionar_dados_planilha_local()
 
-    # Chame a função para calcular as datas de envio de SMS e obter informações adicionais
-    infos_envio_sms = calculo_parcelas()
-
-    if infos_envio_sms:
-        print("As seguintes informações de envio de SMS de cobrança devem ser enviadas hoje:")
-        for info in infos_envio_sms:
-            nome, telefone, valor, numero = info
-            print(f"Nome: {nome}, Telefone: {telefone}, Valor da Parcela: R${valor}, Parcela n°: {numero}")
+    clientes_match = identificar_parcelas()
+    if clientes_match:
+        for cliente in clientes_match:
+            print(f"Nome: {cliente['Nome']}")
+            print(f"Celular: {cliente['Celular']}")
+            print(f"Parcela: {cliente['Parcela']}")
+            print(f"Valor da Parcela: {cliente['Valor da Parcela']}")
+            print("-" * 30)
+            enviar_sms_cliente(  # Mover a chamada para enviar_sms_cliente() aqui
+                nome_cliente=cliente['Nome'],
+                numero_celular=cliente['Celular'],
+                numero_parcela=cliente['Parcela'],
+                valor_parcela=cliente['Valor da Parcela']
+            )
     else:
-        print("Nenhuma informação de envio de SMS de cobrança para hoje.")
+        print("Nenhum cliente corresponde aos critérios.")
