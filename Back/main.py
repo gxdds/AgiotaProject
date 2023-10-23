e9b0fb8452b3f4d0cb9884afa91fb78aac722678
Vinícius Gadioli <viniciusgadioli2@gmail.com>
2023-10-23 18:27:01 -0300

    func1

--- Back/main.py
+++ Back/main.py
@@ -4,20 +4,37 @@ import openpyxl
 
 if __name__ == "__main__":
     # Colete os dados do cliente
-    cliente, nmr_cliente, valor, porcentagem, intervalo_pagamentos, parcelas, formatted_valor_parcela, total_a_pagar, data_formatada = coletar_dados_cliente()
+    #cliente, nmr_cliente, valor, porcentagem, intervalo_pagamentos, parcelas, formatted_valor_parcela, total_a_pagar, data_formatada = coletar_dados_cliente()
 
 
     # Adicione os dados à planilha local
-    adicionar_dados_planilha_local()
+    #adicionar_dados_planilha_local()
 
+    clientes_com_sms_hoje = []
     clientes_match = identificar_parcelas()
+
     if clientes_match:
         for cliente in clientes_match:
-            print(f"Nome: {cliente['Nome']}")
-            print(f"Celular: {cliente['Celular']}")
-            print(f"Parcela: {cliente['Parcela']}")
-            print(f"Valor da Parcela: {cliente['Valor da Parcela']}")
-            print("-" * 30)
-            enviar_sms_cliente(nome_cliente=cliente['Nome'], numero_celular=cliente['Celular'], numero_parcela=cliente['Parcela'], valor_parcela=cliente['Valor da Parcela'])
+            # Extraia as informações relevantes do cliente
+            nome = cliente['Nome']
+            celular = cliente['Celular']
+            parcela = cliente['Parcela']
+            valor_parcela = cliente['Valor da Parcela']
+
+            # Calcule o valor total com base no número da parcela
+            valor_total = parcela * valor_parcela
+
+            # Adicione as informações do cliente à lista
+            cliente_info = {
+                'Nome': nome,
+                'Celular': celular,
+                'Parcela': parcela,
+                'Valor da Parcela': valor_parcela,
+                'Valor Total': valor_total
+            }
+            clientes_com_sms_hoje.append(cliente_info)
+
+    if clientes_com_sms_hoje:
+        print(clientes_com_sms_hoje)
     else:
-        print("Nenhum cliente corresponde aos critérios.")
+        print("Nenhum cliente para enviar SMS hoje.")
